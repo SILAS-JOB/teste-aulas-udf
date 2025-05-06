ce17268e0981fd90157bd1839e84699a1a28aeeb
LUIS EDUARDO DA SILVA LIMA <luis.lima004@cs.udf.edu.br>
2025-05-06 20:22:17 -0300

    aula 2 ou 3

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log("Hello, World!");
-console.log("This is a test script.");
+const toggleBtn = document.getElementById('toggle-dark');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i> Modo Claro';
+  } else {
+    toggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i> Modo Escuro';
+  }
+});
